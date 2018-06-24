d9659ea8f4f8e8f0000d8026f6ba20569e8e6e17
tanyue <417045639@qq.com>
2018-06-24 21:08:19 +0800

    获取网络html数据

---
+++ 获取html数据01.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+'''
+文件说明：
+'''
+import urllib.request
+
+#网址
+url = "http://www.douban.com/"
+
+#请求
+request = urllib.request.Request(url)
+
+#爬取结果
+response = urllib.request.urlopen(request)
+
+data = response.read()
+
+#设置解码方式
+data = data.decode('utf-8')
+
+#打印结果
+print(data)
